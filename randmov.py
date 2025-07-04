db4c434b82af87555fa1e89da3fd28833b9bbf5f
Pablo Gutiérrez <pablogf@uma.es>
2025-07-04 21:54:48 +0200

    added login
    Signed-off-by: Pablo Gutiérrez <pablogf@uma.es>

--- randmov.py
+++ randmov.py
@@ -1,8 +1,12 @@
 import chromedriver_autoinstaller
-chromedriver_autoinstaller.install() # Installs latest version of ChromeDriver and skips if allready installed
+chromedriver_autoinstaller.install() # Installs latest version of ChromeDriver and skips if already installed
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 import yaml
+
+# Target URLs
+LOGIN_URL = "https://letterboxd.com/sign-in/"
+DATA_URL = "https://letterboxd.com/settings/data/"
 
 # Read credentials
 conf = yaml.safe_load(open('login_details.yml'))
@@ -22,4 +26,23 @@ def login(url, usernameId, username, passwordId, password, submit_buttonId):
     driver.find_element(By.CSS_SELECTOR, submit_buttonId).click()
 
 # Run it
-login("https://letterboxd.com/sign-in/", "field-username", username, "field-password", password, "button.standalone-flow-button.-inline.-action.-activity-indicator")
+login(LOGIN_URL, "field-username", username, "field-password", password, "button.standalone-flow-button.-inline.-action.-activity-indicator")
+
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+# Click on "settings"
+
+
+# Click on "data"
+
+
+# Click on the "Export data" button
+WebDriverWait(driver, 10).until(
+    EC.element_to_be_clickable((By.CSS_SELECTOR, "export-data-link cboxElement button"))
+).click()
+
+# Click on "Export data" button in the pop-up window
+WebDriverWait(driver, 10).until(
+    EC.element_to_be_clickable((By.CSS_SELECTOR, "button.-action.button-action.export-data-button"))
+).click()
